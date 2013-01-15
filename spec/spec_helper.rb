e081f587183c8ed53c59744d57cbb99f5a085be9
Keiichiro Nishi <ktouth@k-brand.gr.jp>
2013-01-15 21:16:00 +0900

    Coverageツールの変更

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,12 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
+require 'simplecov'
+
+SimpleCov.start do
+  add_filter 'spec'
+end
+
 require 'ktouth-st2'
 
 # Requires supporting files with custom matchers and macros, etc,
