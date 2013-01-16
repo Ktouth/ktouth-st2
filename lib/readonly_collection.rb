2b8227aa6647af3806f0cba32919f212bb2825ac
Keiichiro Nishi <ktouth@k-brand.gr.jp>
2013-01-16 23:04:10 +0900

    KtouthBrand::ReadOnlyCollection は Enumerable をインクルードしている

--- lib/readonly_collection.rb
+++ lib/readonly_collection.rb
@@ -1,8 +1,16 @@
 #-- coding: UTF-8 --
+require 'forwardable'
+
 module KtouthBrand
   class ReadOnlyCollection
+    include Enumerable
+
     def initialize(array)
       raise ArgumentError, 'ソースとなる配列が指定されていません' unless array.kind_of?(Enumerable)
+      @source = array
     end
+
+    extend Forwardable
+    def_delegator :@source, :each    
   end
 end
--- spec/readonly_collection_spec.rb
+++ spec/readonly_collection_spec.rb
@@ -3,6 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 describe "KtouthBrand::ReadOnlyCollection" do
   subject { KtouthBrand::ReadOnlyCollection }
   it { should be_instance_of(Class) }
+  it { should < Enumerable }
 
   before :all do
     @source = [:sample, "test", /is/, 12345]
@@ -15,5 +16,15 @@ describe "KtouthBrand::ReadOnlyCollection" do
     it { expect { subject.new(/test/) }.to raise_error(ArgumentError) }
     it { expect { subject.new('189231') }.to raise_error(ArgumentError) }
     it { expect { subject.new(nil) }.to raise_error(ArgumentError) }
+  end
+
+  describe '#each' do
+    before :all do
+      @object = KtouthBrand::ReadOnlyCollection.new(@source)
+    end
+    subject { @object }
+
+    it { should respond_to(:each) }
+    it { subject.to_a.should == @source }    
   end
 end
