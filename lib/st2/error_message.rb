faff84747df4a6fb34eaa2ccffe3dba77d6db058
Keiichiro Nishi <ktouth@k-brand.gr.jp>
2013-01-18 17:51:40 +0900

    KtouthBrand::ST2::ErrorMessage#line 属性の追加

--- lib/st2/error_message.rb
+++ lib/st2/error_message.rb
@@ -4,10 +4,12 @@ module KtouthBrand
       def initialize(message, options = {})
         raise ArgumentError, 'message is invalid text' unless message.kind_of?(String)
         raise ArgumentError, 'invalid option parameter' unless options.kind_of?(Hash)
-        
-        @message = message
+        line = options.delete(:line)
+        raise ArgumentError, 'line options is integer only' unless line.nil? || line.kind_of?(Integer)
+
+        @message, @line = message, line
       end
-      attr_reader :message
+      attr_reader :message, :line
     end
   end
 end
--- spec/st2/error_message_spec.rb
+++ spec/st2/error_message_spec.rb
@@ -3,6 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe "KtouthBrand::ST2::ErrorMessage" do
   before :all do
     @text = 'sample error message!'
+    @lineno = 152
   end
 
   subject { KtouthBrand::ST2::ErrorMessage }
@@ -14,6 +15,9 @@ describe "KtouthBrand::ST2::ErrorMessage" do
       it { expect { subject.new(/invalid/) }.to raise_error(ArgumentError) }
       it { expect { subject.new(15623) }.to raise_error(ArgumentError) }
       it { expect { subject.new('simple', /invalid/) }.to raise_error(ArgumentError) }
+
+      it { expect { subject.new(@text, :line => /invalid/) }.to raise_error(ArgumentError) }
+      it { expect { subject.new(@text, :line => 'this option is integer only') }.to raise_error(ArgumentError) }
     end
     it { subject.new('valid error-message.', :line => 10, :column => 19).should be_a(subject) }
   end
@@ -22,5 +26,16 @@ describe "KtouthBrand::ST2::ErrorMessage" do
     subject { KtouthBrand::ST2::ErrorMessage.new(@text) }
     it { should be_respond_to(:message) }
     it { subject.message.should == @text }
+  end
+
+  describe "#line" do
+    subject { KtouthBrand::ST2::ErrorMessage.new(@text) }
+    it { should be_respond_to(:line) }
+    it { subject.line.should be_nil }
+    
+    context 'set :lineno option' do
+      subject { KtouthBrand::ST2::ErrorMessage.new(@text, :line => @lineno) }
+      it { subject.line.should == @lineno }
+    end
   end
 end
