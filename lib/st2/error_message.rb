4a7a479c4115c11553343ecb6ddc4d2086e8d9d2
Keiichiro Nishi <ktouth@k-brand.gr.jp>
2013-01-18 17:39:20 +0900

    KtouthBrand::ST2::ErrorMessage(#initialize) の実装

--- lib/st2/error_message.rb
+++ lib/st2/error_message.rb
@@ -1,7 +1,10 @@
 module KtouthBrand
   module ST2
     class ErrorMessage
-      
+      def initialize(message, options = {})
+        raise ArgumentError, 'message is invalid text' unless message.kind_of?(String)
+        raise ArgumentError, 'invalid option parameter' unless options.kind_of?(Hash)
+      end
     end
   end
 end
--- spec/st2/error_message_spec.rb
+++ spec/st2/error_message_spec.rb
@@ -3,4 +3,14 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe "KtouthBrand::ST2::ErrorMessage" do
   subject { KtouthBrand::ST2::ErrorMessage }
   it { should be_instance_of(Class) }
+
+  describe ".new" do
+    context 'argument exceptions' do
+      it { expect { subject.new }.to raise_error(ArgumentError) }
+      it { expect { subject.new(/invalid/) }.to raise_error(ArgumentError) }
+      it { expect { subject.new(15623) }.to raise_error(ArgumentError) }
+      it { expect { subject.new('simple', /invalid/) }.to raise_error(ArgumentError) }
+    end
+    it { subject.new('valid error-message.', :line => 10, :column => 19).should be_a(subject) }
+  end
 end
