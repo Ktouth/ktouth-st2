59d26062b36e47d4885d1e0b471d5ec74ac949fd
Keiichiro Nishi <ktouth@k-brand.gr.jp>
2013-01-25 17:44:59 +0900

    KtouthBrand::ST2::Node#__source_line__ / #__source_column__ プロパティの追加

--- lib/st2/node.rb
+++ lib/st2/node.rb
@@ -3,5 +3,10 @@ module KtouthBrand::ST2
     class <<self
       private :new
     end
+
+    def initialize
+      @__source_line__ = @__source_column__ = nil
+    end
+    attr_accessor :__source_line__, :__source_column__
   end
 end
--- spec/st2/node_spec.rb
+++ spec/st2/node_spec.rb
@@ -4,4 +4,28 @@ describe "KtouthBrand::ST2::Node" do
   subject { KtouthBrand::ST2::Node }
   it { should be_instance_of(Class) }
   it { should_not be_respond_to(:new) }
+
+  describe '#__source_line__' do
+    before do
+      @node = KtouthBrand::ST2::Node.send :new
+    end
+    subject { @node }
+    
+    it { should be_respond_to(:__source_line__) }
+    it { should be_respond_to(:__source_line__=) }
+    it { subject.__source_line__.should be_nil }
+    it { expect { subject.__source_line__ = 9453 }.to change { subject.__source_line__ }.to(9453) }
+  end
+
+  describe '#__source_column__' do
+    before do
+      @node = KtouthBrand::ST2::Node.send :new
+    end
+    subject { @node }
+    
+    it { should be_respond_to(:__source_column__) }
+    it { should be_respond_to(:__source_column__=) }
+    it { subject.__source_column__.should be_nil }
+    it { expect { subject.__source_column__ = 122 }.to change { subject.__source_column__ }.to(122) }
+  end
 end
