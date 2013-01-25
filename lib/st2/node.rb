dbc87ef5e378270fafe78325b78c483c78900796
Keiichiro Nishi <ktouth@k-brand.gr.jp>
2013-01-25 17:56:08 +0900

    KtouthBrand::ST2::Node(#make_error_message) プライベートメソッドの追加

--- lib/st2/node.rb
+++ lib/st2/node.rb
@@ -8,5 +8,11 @@ module KtouthBrand::ST2
       @__source_line__ = @__source_column__ = nil
     end
     attr_accessor :__source_line__, :__source_column__
+
+    private
+
+    def make_error_message(message)
+      ErrorMessage.new(message, :line => __source_line__, :column => __source_column__)
+    end
   end
 end
--- spec/st2/node_spec.rb
+++ spec/st2/node_spec.rb
@@ -28,4 +28,27 @@ describe "KtouthBrand::ST2::Node" do
     it { subject.__source_column__.should be_nil }
     it { expect { subject.__source_column__ = 122 }.to change { subject.__source_column__ }.to(122) }
   end
+  
+  describe '(#make_error_message)' do
+    def mk(line, column)
+      KtouthBrand::ST2::Node.send(:new).tap do |o|
+        o.__source_line__ = line
+        o.__source_column__ = column
+      end
+    end
+    def call(n, *args, &block); n.send :make_error_message, *args, &block end 
+    before do
+      @node = KtouthBrand::ST2::Node.send :new
+      @nodeA, @nodeB, @nodeC = mk(15, 34), mk(nil, 5), mk(153, nil)
+    end
+    subject { @node }
+    
+    it { should_not be_respond_to(:make_error_message) }
+    it { should be_respond_to(:make_error_message, true) }
+    it { call(@node, 'test').should be_a(KtouthBrand::ST2::ErrorMessage) }
+    it { call(@node, 'test').to_s == '-:-:test' }
+    it { call(@nodeA, 'test').to_s == '15:34:test' }
+    it { call(@nodeB, 'test').to_s == '-:5:test' }
+    it { call(@nodeC, 'test').to_s == '153:-:test' }
+  end
 end
