ae7682d242635d425891f5f16ee64697c9153e32
Keiichiro Nishi <ktouth@k-brand.gr.jp>
2013-02-06 20:20:59 +0900

    KtouthBrand::ST2::NodeFormatterContext(#initialize) の追加

--- lib/st2/node_formatter_context.rb
+++ lib/st2/node_formatter_context.rb
@@ -4,5 +4,10 @@ module KtouthBrand::ST2
       private :new
     end
 
+    def initialize(formatter, parent = nil)
+      raise ArgumentError, 'formatter is not NodeFormatter' unless formatter.is_a?(NodeFormatter)
+      raise ArgumentError, 'parent is not NodeFormatterContext' unless parent.nil? || parent.is_a?(NodeFormatterContext)
+      @formatter, @parent = formatter, parent
+    end
   end
 end
--- spec/st2/node_formatter_context_spec.rb
+++ spec/st2/node_formatter_context_spec.rb
@@ -4,4 +4,33 @@ describe "KtouthBrand::ST2::NodeFormatterContext" do
   subject { KtouthBrand::ST2::NodeFormatterContext }
   it { should be_instance_of(Class) }
   it { should_not be_respond_to(:new) }
+
+  def create(*args); KtouthBrand::ST2::NodeFormatterContext.send(:new, *args) end
+
+  describe "(#initialize)" do
+    before :all do
+      @formatter = KtouthBrand::ST2::NodeFormatter.send(:new)
+      @parent = KtouthBrand::ST2::NodeFormatterContext.allocate
+    end
+    it { expect { create }.to raise_error }
+    it { expect { create(nil) }.to raise_error }
+    it { expect { create(153132) }.to raise_error }
+    it { expect { create("nil") }.to raise_error }
+    it { expect { create(/test/) }.to raise_error }
+    it { expect { create(@formatter) }.to_not raise_error }
+    
+    it { expect { create(@formatter, 336) }.to raise_error }
+    it { expect { create(@formatter, 'nil') }.to raise_error }
+    it { expect { create(@formatter, /invalid/) }.to raise_error }
+    it { expect { create(@formatter, @parent) }.to_not raise_error }
+    
+    context 'valid created' do
+      subject { create(@formatter) }
+      it { subject.instance_variable_get(:@formatter).should == @formatter }
+    end
+    context 'valid created with parent context' do
+      subject { create(@formatter, @parent) }
+      it { subject.instance_variable_get(:@parent).should == @parent }
+    end
+  end
 end
