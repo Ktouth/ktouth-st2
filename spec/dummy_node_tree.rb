47f724d4a12cc98dde526b1d73ece88ff4a34331
Keiichiro Nishi <ktouth@k-brand.gr.jp>
2013-02-11 12:14:47 +0900

    'tree nodes' 共有コンテキストを拡充、独立したヘルパーファイルに分割

---
+++ spec/dummy_node_tree.rb
@@ -0,0 +1,70 @@
+class ExampleNode < KtouthBrand::ST2::Node
+  class <<self
+    public :new
+  end
+  def initialize(_id)
+    super()
+    @id = _id
+  end
+  attr_reader :id
+  def inspect; "<Example: #{id}>" end
+  def each_error_message; end
+end
+def create_nodes(num); num.to_i.times.map {|x| ExampleNode.new(x) } end
+def assign_each_node(owner, array, *indexes)
+  klass = class <<owner; self end
+  klass.send(:define_method, :each_node) do |&block|
+    indexes.each {|x| block.call(array[x]) }
+  end
+end
+
+shared_context 'tree nodes' do
+  #  1 -   4 -  11
+  #             12 -  22
+  #        5
+  #        6 -  13
+  #  2 -   7 -  14
+  #             15 -  23 -  31
+  #                         32 -  33 -  34
+  #                                     35
+  #                   24
+  #                   25
+  #             16
+  #             17
+  #             18 -  26
+  #                   27
+  #             19
+  #        8 -  20 -  28
+  #                   29
+  #                   30
+  #        9
+  #  3 -  10 -  21
+  before do
+    @nodes = create_nodes(36)
+    assign_each_node(@nodes[33], @nodes, 34, 35)
+    assign_each_node(@nodes[32], @nodes, 33)
+    assign_each_node(@nodes[23], @nodes, 31, 32)
+    assign_each_node(@nodes[20], @nodes, 28, 29, 30)
+    assign_each_node(@nodes[18], @nodes, 26, 27)
+    assign_each_node(@nodes[15], @nodes, 23, 24, 25)
+    assign_each_node(@nodes[12], @nodes, 22)
+    assign_each_node(@nodes[10], @nodes, 21)
+    assign_each_node(@nodes[ 8], @nodes, 20)
+    assign_each_node(@nodes[ 7], @nodes, 14, 15, 16, 17, 18, 19)
+    assign_each_node(@nodes[ 6], @nodes, 13)
+    assign_each_node(@nodes[ 4], @nodes, 11, 12)
+    assign_each_node(@nodes[ 3], @nodes, 10)
+    assign_each_node(@nodes[ 2], @nodes, 7, 8, 9)
+    assign_each_node(@nodes[ 1], @nodes, 4, 5, 6)
+    assign_each_node(@nodes[ 0], @nodes, 1, 2, 3)
+    @tree_current_indexes = [0, 1, 4, 11, 12, 22, 5, 6, 13, 2, 7, 14, 15, 23, 31, 32, 33, 34, 35, 24, 25, 16, 17, 18, 26, 27, 19, 8, 20, 28, 29, 30, 9, 3, 10, 21]
+    @tree_before_indexes  = [nil, nil, nil, nil, 11, nil, 4, 5, nil, 1, nil, nil, 14, nil, nil, 31, nil, nil, 34, 23, 24, 15, 16,17, nil, 26, 18, 7, nil, nil, 28, 29, 8, 2, nil, nil]
+    @tree_after_indexes   = [nil, 2, 5, 12, nil, nil, 6, nil, nil, 3, 8, 15, 16, 24, 32, nil, nil, 35, nil, 25, nil, 17, 18, 19, 27, nil, nil, 9, nil, 29, 30, nil, nil, nil, nil, nil]
+    @tree_parent_indexes  = [nil, 0, 1, 4, 4, 12, 1, 1, 6, 0, 7, 7, 15, 23, 23, 32, 33, 33, 15, 15, 7, 7, 7, 18, 18, 7, 2, 8, 20, 20, 20, 2, 0, 2, 10]
+  end
+end
+
+def assign_each_error_message(node, &block)
+  klass = class <<node; self end
+  klass.send(:define_method, :each_error_message, &block)
+end
--- spec/st2/node_validator_spec.rb
+++ spec/st2/node_validator_spec.rb
@@ -1,4 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/../dummy_node_tree')
 
 describe "KtouthBrand::ST2::NodeValidator" do
   subject { KtouthBrand::ST2::NodeValidator }
@@ -27,44 +28,6 @@ describe "KtouthBrand::ST2::NodeValidator" do
     it { expect { subject.add_message(KtouthBrand::ST2::ErrorMessage.new('error')) }.to change { subject.size }.from(0).to(1) } 
   end
 
-  class ExampleNode < KtouthBrand::ST2::Node
-    class <<self
-      public :new
-    end
-    def initialize(_id)
-      super()
-      @id = _id
-    end
-    attr_reader :id
-    def inspect; "<Example: #{id}>" end
-    def each_error_message; end
-  end
-  def create_nodes(num); num.to_i.times.map {|x| ExampleNode.new(x) } end
-  def assign_each_node(owner, array, *indexes)
-    klass = class <<owner; self end
-    klass.send(:define_method, :each_node) do |&block|
-      indexes.each {|x| block.call(array[x]) }
-    end
-  end
-  def assign_each_error_message(node, &block)
-    klass = class <<node; self end
-    klass.send(:define_method, :each_error_message, &block)
-  end
-
-  shared_context 'tree nodes' do
-    # tree => 0 =>[1, 2 => [3 => [4, 5], 6, 7 => [8 => [9]]], 10 => [11 => [12, 13], 14, 15]]
-    before do
-      @nodes = create_nodes(16)
-      assign_each_node(@nodes[0], @nodes, 1, 2, 10)
-      assign_each_node(@nodes[2], @nodes, 3, 6, 7)
-      assign_each_node(@nodes[3], @nodes, 4, 5)
-      assign_each_node(@nodes[7], @nodes, 8)
-      assign_each_node(@nodes[8], @nodes, 9)
-      assign_each_node(@nodes[10], @nodes, 11, 14, 15)
-      assign_each_node(@nodes[11], @nodes, 12, 13)
-    end
-  end
-
   describe '#validate' do
     subject { KtouthBrand::ST2::NodeValidator.new }
     
@@ -85,7 +48,7 @@ describe "KtouthBrand::ST2::NodeValidator" do
         @nodes.each do |n|
           assign_each_error_message(n) { result.push self.inspect }
         end
-        @examples = @nodes.map(&:inspect)
+        @examples = @tree_current_indexes.map {|x| @nodes[x].inspect }
       end
       it { expect { subject.validate(@nodes[0]) }.to change { @result }.to(@examples) }
     end
