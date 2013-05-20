b95ecc84c3a37316ff806b464dce11b8e41a09b5
Keiichiro Nishi <ktouth@k-brand.gr.jp>
2013-05-21 08:06:40 +0900

    KtouthBrand::ST2::Section#blocks / #add_block / #remove_block の追加

--- lib/st2/node/section.rb
+++ lib/st2/node/section.rb
@@ -5,6 +5,21 @@ module KtouthBrand::ST2
     end
 
     def initialize
+      @blocks = KtouthBrand::ReadOnlyCollection.new(@blocks_impl = [])
+    end
+    attr_reader :blocks
+
+    def add_block(first_node, *nodes)
+      nodes.unshift first_node
+      raise ArgumentError, 'nodes contained not Node instance.' unless nodes.all? {|x| (x.class < Node::Block) }
+      @blocks_impl.concat(nodes).uniq!
+      self
+    end
+
+    def remove_block(node)
+      raise ArgumentError, 'node is not Node::Inline instance.' unless node.class < Node::Block
+      @blocks_impl.delete(node)
+      self
     end
 
     def each_error_message
--- spec/st2/node/section_spec.rb
+++ spec/st2/node/section_spec.rb
@@ -5,9 +5,90 @@ describe "KtouthBrand::ST2::Section" do
   before :all do
     @node_type = KtouthBrand::ST2::Section
     @valid_node = @node_type.new
+    @valid_children = [
+      KtouthBrand::ST2::Paragraph.new.add_inline(
+        KtouthBrand::ST2::Text.new('test'), KtouthBrand::ST2::NewLine.new, KtouthBrand::ST2::Text.new('valid'), KtouthBrand::ST2::Text.new('ok!!', :pre_blank => true)
+      ),
+      KtouthBrand::ST2::Separator.new,
+      KtouthBrand::ST2::Paragraph.new.add_inline(
+        KtouthBrand::ST2::Text.new('simple-text.', :pre_blank => true)
+      ),
+      KtouthBrand::ST2::Paragraph.new.add_inline(
+        KtouthBrand::ST2::Text.new('find'), KtouthBrand::ST2::Text.new('valid!!', :pre_blank => true)
+      ),
+    ]
   end
   include_context "node class specset"
 
   it { @node_type.should_not < KtouthBrand::ST2::Node::Inline }
   it { @node_type.should_not < KtouthBrand::ST2::Node::Block }
+
+  describe '#blocks' do
+    before do
+      @sec = @node_type.new
+    end
+    subject { @sec }
+    
+    it { should be_respond_to(:blocks) }
+    it { subject.blocks.should be_a(KtouthBrand::ReadOnlyCollection) }
+    it { subject.blocks.empty?.should be_true }
+  end
+
+  describe '#add_block' do
+    before do
+      @sec = @node_type.new
+    end
+    subject { @sec }
+
+    it { should be_respond_to(:add_block) }
+    it { expect { subject.add_block }.to raise_error(ArgumentError) }
+    it { expect { subject.add_block(@valid_children.first) }.to_not raise_error }
+    it { expect { subject.add_block(*@valid_children) }.to_not raise_error }
+    it { expect { subject.add_block(nil) }.to raise_error }
+    it { expect { subject.add_block(:abort) }.to raise_error }
+    it { expect { subject.add_block('nil') }.to raise_error }
+    it { expect { subject.add_block(123589) }.to raise_error }
+    it { expect { subject.add_block(@valid_children.last, KtouthBrand::ST2::NewLine.new) }.to raise_error }
+    it { expect { subject.add_block(*@valid_children[0..2], KtouthBrand::ST2::Node::Block.send(:new), *@valid_children.last) }.to raise_error }
+
+    it { subject.add_block(*@valid_children).should == subject }
+    it { expect { subject.add_block(*@valid_children) }.to change { subject.blocks.size }.from(0).to(@valid_children.size) }
+    it { expect { subject.add_block(*@valid_children) }.to change { subject.blocks.to_a }.from([]).to(@valid_children) }
+
+    context 'is not add already exist item' do
+      before do
+        @sec.add_block(*@valid_children)
+      end
+      it { expect { subject.add_block(@valid_children[-1]) }.to_not change { subject.blocks.to_a } }
+    end
+    context 'is not add duplicated item' do
+      before do
+        @duplicated = [0, 1, 1, 2, 3, 3, 3].map {|x| @valid_children[x] }
+      end
+      it { expect { subject.add_block(*@duplicated) }.to change { subject.blocks.to_a }.to(@valid_children) }
+    end
+  end  
+
+  describe '#remove_block' do
+    before do
+      @sec = @node_type.new.add_block(*@valid_children)
+    end
+    subject { @sec }
+
+    it { should be_respond_to(:remove_block) }
+    it { expect { subject.remove_block }.to raise_error(ArgumentError) }
+    it { expect { subject.remove_block(@valid_children.first) }.to_not raise_error }
+    it { expect { subject.remove_block(*@valid_children) }.to raise_error }
+    it { expect { subject.remove_block(nil) }.to raise_error }
+    it { expect { subject.remove_block(:abort) }.to raise_error }
+    it { expect { subject.remove_block('nil') }.to raise_error }
+    it { expect { subject.remove_block(123589) }.to raise_error }
+    it { expect { subject.remove_block(KtouthBrand::ST2::Text.new) }.to raise_error }
+    it { expect { subject.remove_block(KtouthBrand::ST2::Node::Block.send(:new)) }.to raise_error }
+    it { expect { subject.remove_block(KtouthBrand::ST2::Separator.new) }.to_not raise_error }
+
+    it { subject.remove_block(@valid_children.first).should == subject }
+    it { expect { subject.remove_block(@valid_children[2]) }.to change { subject.blocks.size }.from(@valid_children.size).by(-1) }
+    it { expect { subject.remove_block(@valid_children[1]) }.to change { subject.blocks.to_a }.from(@valid_children).to([@valid_children.first] + @valid_children[2..-1]) }
+  end
 end
