2f4a21bc3805142428e73a8815f5c60957a11067
Keiichiro Nishi <ktouth@k-brand.gr.jp>
2013-05-21 08:31:05 +0900

    KtouthBrand::ST2::Section#title_texts / #add_title_text / #remove_title_text の追加

--- lib/st2/node/section.rb
+++ lib/st2/node/section.rb
@@ -6,8 +6,9 @@ module KtouthBrand::ST2
 
     def initialize
       @blocks = KtouthBrand::ReadOnlyCollection.new(@blocks_impl = [])
+      @title_texts = KtouthBrand::ReadOnlyCollection.new(@title_texts_impl = [])
     end
-    attr_reader :blocks
+    attr_reader :blocks, :title_texts
 
     def add_block(first_node, *nodes)
       nodes.unshift first_node
@@ -17,8 +18,21 @@ module KtouthBrand::ST2
     end
 
     def remove_block(node)
-      raise ArgumentError, 'node is not Node::Inline instance.' unless node.class < Node::Block
+      raise ArgumentError, 'node is not Node::Block instance.' unless node.class < Node::Block
       @blocks_impl.delete(node)
+      self
+    end
+
+    def add_title_text(first_node, *nodes)
+      nodes.unshift first_node
+      raise ArgumentError, 'nodes contained not Node instance.' unless nodes.all? {|x| x.is_a?(Text) }
+      @title_texts_impl.concat(nodes).uniq!
+      self
+    end
+
+    def remove_title_text(node)
+      raise ArgumentError, 'node is not Text instance.' unless node.is_a?(Text)
+      @title_texts_impl.delete(node)
       self
     end
 
--- spec/st2/node/section_spec.rb
+++ spec/st2/node/section_spec.rb
@@ -17,6 +17,7 @@ describe "KtouthBrand::ST2::Section" do
         KtouthBrand::ST2::Text.new('find'), KtouthBrand::ST2::Text.new('valid!!', :pre_blank => true)
       ),
     ]
+    @valid_title_texts = [KtouthBrand::ST2::Text.new('title'), KtouthBrand::ST2::Text.new('valid', :pre_blank => true), KtouthBrand::ST2::Text.new('ok!!', :pre_blank => true), KtouthBrand::ST2::Text.new('prease.', :pre_blank => true), ]
   end
   include_context "node class specset"
 
@@ -90,5 +91,77 @@ describe "KtouthBrand::ST2::Section" do
     it { subject.remove_block(@valid_children.first).should == subject }
     it { expect { subject.remove_block(@valid_children[2]) }.to change { subject.blocks.size }.from(@valid_children.size).by(-1) }
     it { expect { subject.remove_block(@valid_children[1]) }.to change { subject.blocks.to_a }.from(@valid_children).to([@valid_children.first] + @valid_children[2..-1]) }
+  end
+
+  describe '#title_texts' do
+    before do
+      @sec = @node_type.new
+    end
+    subject { @sec }
+    
+    it { should be_respond_to(:title_texts) }
+    it { subject.title_texts.should be_a(KtouthBrand::ReadOnlyCollection) }
+    it { subject.title_texts.empty?.should be_true }
+    it { subject.title_texts.should_not == subject.blocks }
+  end
+
+  describe '#add_title_text' do
+    before do
+      @sec = @node_type.new
+    end
+    subject { @sec }
+
+    it { should be_respond_to(:add_title_text) }
+    it { expect { subject.add_title_text }.to raise_error(ArgumentError) }
+    it { expect { subject.add_title_text(@valid_title_texts.first) }.to_not raise_error }
+    it { expect { subject.add_title_text(*@valid_title_texts) }.to_not raise_error }
+    it { expect { subject.add_title_text(nil) }.to raise_error }
+    it { expect { subject.add_title_text(:abort) }.to raise_error }
+    it { expect { subject.add_title_text('nil') }.to raise_error }
+    it { expect { subject.add_title_text(123589) }.to raise_error }
+    it { expect { subject.add_title_text(@valid_title_texts.last, KtouthBrand::ST2::Paragraph.new) }.to raise_error }
+    it { expect { subject.add_title_text(@valid_title_texts.last, KtouthBrand::ST2::NewLine.new) }.to raise_error }
+    it { expect { subject.add_title_text(*@valid_title_texts[0..2], KtouthBrand::ST2::Node::Inline.send(:new), *@valid_title_texts.last) }.to raise_error }
+
+    it { subject.add_title_text(*@valid_title_texts).should == subject }
+    it { expect { subject.add_title_text(*@valid_title_texts) }.to change { subject.title_texts.size }.from(0).to(@valid_title_texts.size) }
+    it { expect { subject.add_title_text(*@valid_title_texts) }.to change { subject.title_texts.to_a }.from([]).to(@valid_title_texts) }
+
+    context 'is not add already exist item' do
+      before do
+        @sec.add_title_text(*@valid_title_texts)
+      end
+      it { expect { subject.add_title_text(@valid_title_texts[-1]) }.to_not change { subject.title_texts.to_a } }
+    end
+    context 'is not add duplicated item' do
+      before do
+        @duplicated = [0, 1, 1, 2, 3, 3, 3].map {|x| @valid_title_texts[x] }
+      end
+      it { expect { subject.add_title_text(*@duplicated) }.to change { subject.title_texts.to_a }.to(@valid_title_texts) }
+    end
+  end  
+
+  describe '#remove_title_text' do
+    before do
+      @sec = @node_type.new.add_title_text(*@valid_title_texts)
+    end
+    subject { @sec }
+
+    it { should be_respond_to(:remove_title_text) }
+    it { expect { subject.remove_title_text }.to raise_error(ArgumentError) }
+    it { expect { subject.remove_title_text(@valid_title_texts.first) }.to_not raise_error }
+    it { expect { subject.remove_title_text(*@valid_title_texts) }.to raise_error }
+    it { expect { subject.remove_title_text(nil) }.to raise_error }
+    it { expect { subject.remove_title_text(:abort) }.to raise_error }
+    it { expect { subject.remove_title_text('nil') }.to raise_error }
+    it { expect { subject.remove_title_text(123589) }.to raise_error }
+    it { expect { subject.remove_title_text(KtouthBrand::ST2::Paragraph.new) }.to raise_error }
+    it { expect { subject.remove_title_text(KtouthBrand::ST2::NewLine.new) }.to raise_error }
+    it { expect { subject.remove_title_text(KtouthBrand::ST2::Node::Inline.send(:new)) }.to raise_error }
+    it { expect { subject.remove_title_text(KtouthBrand::ST2::Text.new('not found')) }.to_not raise_error }
+
+    it { subject.remove_title_text(@valid_title_texts.first).should == subject }
+    it { expect { subject.remove_title_text(@valid_title_texts[2]) }.to change { subject.title_texts.size }.from(@valid_title_texts.size).by(-1) }
+    it { expect { subject.remove_title_text(@valid_title_texts[1]) }.to change { subject.title_texts.to_a }.from(@valid_title_texts).to([@valid_title_texts.first] + @valid_title_texts[2..-1]) }
   end
 end
