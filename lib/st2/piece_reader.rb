dcaa6b87846e1cdc0e071e084ef13d53b28cc51e
Keiichiro Nishi <ktouth@k-brand.gr.jp>
2013-05-25 07:50:50 +0900

    KtouthBrand::ST2::PieceReader クラスの追加 KtouthBrand::ST2::PieceReader.read / .parse / .open クラスメソッドの追加

--- lib/st2/piece_reader.rb
+++ lib/st2/piece_reader.rb
@@ -1,3 +1,5 @@
+require 'strscan'
+
 module KtouthBrand::ST2
   class Piece # :nodoc:
     class <<self
@@ -8,5 +10,37 @@ module KtouthBrand::ST2
       @token, @lineno, @column, @value = token, lineno, column, value
     end
     attr_reader :token, :lineno, :column, :value
+  end
+
+  class PieceReader #:nodoc:
+    include Enumerable
+
+    class <<self
+      private :new
+
+      def parse(string)
+        raise ArgumentError, 'string is not String' unless string.is_a?(String)
+        new(StringScanner.new(string))
+      end
+
+      def open(filename)
+        raise ArgumentError, 'filename is not String' unless filename.is_a?(String)
+        str = File.open(filename, 'r') {|io| io.read }
+        new(StringScanner.new(str))
+      end
+      
+      def read(stream)
+        raise ArgumentError, 'stream is not respond #read' unless stream.respond_to?(:read)
+        new(StringScanner.new(stream.read))
+      end
+    end
+
+    def initialize(scanner)
+      @scanner = scanner
+    end
+
+    def each
+      raise NotImplemented
+    end
   end
 end
--- spec/st2/piece_reader_spec.rb
+++ spec/st2/piece_reader_spec.rb
@@ -39,3 +39,75 @@ describe "KtouthBrand::ST2::Piece" do
     it { subject.value.should == "foobar" }    
   end  
 end
+
+describe "KtouthBrand::ST2::PieceReader" do
+  subject { KtouthBrand::ST2::PieceReader }
+  it { should be_instance_of(Class) }
+  it { should < Enumerable }
+  it { should_not be_respond_to(:new) }
+
+  def new(*args); KtouthBrand::ST2::PieceReader.send(:new, *args) end
+  def get_scanner(reader); reader.instance_variable_get(:@scanner) end
+
+  describe ".parse" do
+    it { should be_respond_to(:parse) }
+    
+    it { expect { subject.parse() }.to raise_error }
+    it { expect { subject.parse(12354) }.to raise_error }
+    it { expect { subject.parse(nil) }.to raise_error }
+    it { expect { subject.parse(/stest/) }.to raise_error }
+    it { expect { subject.parse(:symbol) }.to raise_error }
+    it { expect { subject.parse(StringIO.new('test')) }.to raise_error }
+    it { expect { subject.parse('valid string') }.to_not raise_error }
+
+    it { subject.parse('test string').should be_a(subject) }
+    it { get_scanner(subject.parse('test string')).should be_a(StringScanner) }
+    it { get_scanner(subject.parse('test string')).string.should == 'test string' }
+  end
+
+  describe ".open" do
+    it { should be_respond_to(:open) }
+    
+    it { expect { subject.open() }.to raise_error }
+    it { expect { subject.open(12354) }.to raise_error }
+    it { expect { subject.open(nil) }.to raise_error }
+    it { expect { subject.open(/stest/) }.to raise_error }
+    it { expect { subject.open(:symbol) }.to raise_error }
+    it { expect { subject.open(StringIO.new('test')) }.to raise_error }
+
+    context 'is open target-file.' do
+      before do
+        @fname, @string = "foobar.txt", "this is valid string."
+        File.should_receive(:open).with(@fname, 'r').once.and_yield(StringIO.new(@string))
+      end
+      it { expect { subject.open(@fname) }.to_not raise_error }
+      it { subject.open(@fname).should be_a(subject) }
+      it { get_scanner(subject.open(@fname)).should be_a(StringScanner) }
+      it { get_scanner(subject.open(@fname)).string.should == @string }
+    end
+  end
+
+  describe ".read" do
+    it { should be_respond_to(:read) }
+
+    it { expect { subject.read() }.to raise_error }
+    it { expect { subject.read(12354) }.to raise_error }
+    it { expect { subject.read(nil) }.to raise_error }
+    it { expect { subject.read(/stest/) }.to raise_error }
+    it { expect { subject.read(:symbol) }.to raise_error }
+    it { expect { subject.read('test') }.to raise_error }
+
+    context 'is read target-stream.' do
+      before do
+        @string = "this is valid string."
+        @obj = Object.new.tap do |t|
+          t.should_receive(:read).with(no_args()).once.and_return(@string)
+        end
+      end
+      it { expect { subject.read(@obj) }.to_not raise_error }
+      it { subject.read(@obj).should be_a(subject) }
+      it { get_scanner(subject.read(@obj)).should be_a(StringScanner) }
+      it { get_scanner(subject.read(@obj)).string.should == @string }
+    end
+  end
+end
